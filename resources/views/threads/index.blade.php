f4030dff1855c0d718e23bf8ac10e69d33dfb94f
aristeinn <aristeinn@gmail.com>
2017-09-15 10:04:56 +0000

    Foreach for threads

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -8,26 +8,20 @@
                 <div class="panel-heading">Forum Threads</div>
 
                 <div class="panel-body">
+                @foreach($threads as $thread)
                     <article>
                         <div>
                             <h4>
-                                Pellentesque iaculis aliquet
+                                {{ $thread['title'] }}
                             </h4>
                         </div>
                         <div class="body">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus convallis lorem a augue fermentum dictum. Sed velit magna, volutpat et fringilla euismod, accumsan sed metus. Aenean placerat vehicula fringilla. Donec accumsan feugiat tincidunt. Integer rutrum nisl enim, et egestas nunc fringilla non. Fusce volutpat euismod felis eget rutrum. Praesent ornare finibus arcu, at fermentum quam iaculis et. Sed rutrum vel tellus vitae dapibus. Cras faucibus consequat fermentum. Ut faucibus, sapien at ullamcorper tempus, nisl arcu convallis libero, sed imperdiet arcu nibh aliquam felis. Vestibulum suscipit ipsum sit amet purus bibendum, ac maximus leo aliquam.
+                        {{ $thread['body'] }}
+
                         </div>
                     </article>
-                    <article>
-                        <div>
-                            <h4>
-                                Etiam auctor lacinia
-                            </h4>
-                        </div>
-                        <div class="body">
-                            Sed posuere purus quis blandit rhoncus. Morbi sed dignissim velit. Quisque a urna sollicitudin neque volutpat suscipit. Suspendisse at porta magna. Nunc dui elit, vulputate eu aliquam ut, rutrum sed odio. Mauris semper mi vitae turpis blandit aliquet eget nec erat. Suspendisse cursus dolor vel quam facilisis fermentum. Nullam id libero elit. Sed mi dolor, lacinia et sapien sed, tincidunt congue sem. Proin dictum tortor lectus, non malesuada metus sollicitudin id.
-                        </div>
-                    </article>
+                    <hr>
+                    @endforeach
                 </div>
             </div>
         </div>
